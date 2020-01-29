abf723ce6ed19c35ea7321a24cd9d425911bb7a1
HernanFerrari <hernan.angel.ferrari@gmail.com>
2020-01-29 17:24:14 -0300

    Agregado de Bullet prefab y su respectivo Script

---
+++ Arteroids/Assets/_Hernan/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletLife = 5;
+
+    private void OnEnable()
+    {
+        StartCoroutine(CounterToDisable());
+    }
+
+    public IEnumerator CounterToDisable() {
+        yield return new WaitForSeconds(bulletLife);
+        gameObject.SetActive(false);
+    }
+}
