e2f428fa67fc5edb09b3cd6fa822d628e82b1e91
HernanFerrari <hernan.angel.ferrari@gmail.com>
2020-01-29 17:25:04 -0300

    Agregado de Script ObjectPooler

---
+++ Arteroids/Assets/_Hernan/Scripts/ObjectPooler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.transform.SetParent(transform);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
