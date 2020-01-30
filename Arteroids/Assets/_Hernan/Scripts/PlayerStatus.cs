1eaf5e6a8dc6c8ed4d263076a3969d0f0602a570
HernanFerrari <hernan.angel.ferrari@gmail.com>
2020-01-30 16:35:15 -0300

    Agregado de PlayerStatus

---
+++ Arteroids/Assets/_Hernan/Scripts/PlayerStatus.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public GameManager gameMgr;
+    private int scoreAmount;
+    public Text scoreTxt;
+
+    public int actualLives;
+    public List<GameObject> lives;
+
+    private void Start()
+    {
+        RestoreLives();
+    }
+
+    public void AddScore(int amount)
+    {
+        scoreAmount += amount;
+        scoreTxt.text = scoreAmount.ToString();
+    }
+
+    public void RestoreLives()
+    {
+        actualLives = lives.Count-1;
+        foreach (GameObject life in lives)
+        {
+            life.SetActive(true);
+        }
+    }
+
+    public void RemoveLife()
+    {
+        lives[actualLives].SetActive(false);
+        if (actualLives > 0)
+        {
+            actualLives--;
+        }
+        else
+        {
+            print("GameOver");
+        }
+
+    }
+}
