7af479900b00a5c4afc3741761ab3da9d66013bb
HernanFerrari <hernan.angel.ferrari@gmail.com>
2020-01-30 16:33:02 -0300

    Agregado de Scripts AsteroidsManager y Asteroid

---
+++ Arteroids/Assets/_Hernan/Scripts/Asteroid.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Type
+{
+    Big,
+    Medium,
+    Small
+}
+
+public class Asteroid : MonoBehaviour
+{
+    public Type type;
+    private AsteroidsManager asteroidMgr;
+
+    private Image image;
+    public List<Sprite> sprites;
+
+    public float minSpeed;
+    public float maxSpeed;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void Initialize(Type t, Vector3 pos, AsteroidsManager asteroidManager)
+    {
+        asteroidMgr = asteroidManager;
+        type = t;
+        SetRandomSprite();
+        SetRandomMovement();
+    }
+
+    public void SetRandomMovement()
+    {
+        Vector2 randVector = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        randVector.Normalize();
+        randVector = randVector * Random.Range(minSpeed, maxSpeed);
+        GetComponent<Rigidbody2D>().AddForce(randVector, ForceMode2D.Impulse);
+    }
+
+    public void SetRandomSprite()
+    {
+        switch ((int)type)
+        {
+            case 0:
+                image.sprite = sprites[Random.Range(0, 4)];
+                image.SetNativeSize();
+                break;
+            case 1:
+                image.sprite = sprites[Random.Range(4, 8)];
+                image.SetNativeSize();
+                break;
+            case 2:
+                image.sprite = sprites[Random.Range(8, 12)];
+                image.SetNativeSize();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            // Seria posible agregar una variable de "puntaje" para cada tipo de Asteroide
+            asteroidMgr.playerStatus.GetComponent<PlayerStatus>().AddScore(10);
+
+            if (type != Type.Small)
+            {
+                asteroidMgr.CreateNewAsteroid(transform.position, type + 1);
+                asteroidMgr.CreateNewAsteroid(transform.position, type + 1);
+            }
+
+            asteroidMgr.RemoveAsteroid(this.gameObject);
+            Destroy(gameObject);
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision.tag == "Player")
+        {
+            asteroidMgr.ResetAsteroids();
+            asteroidMgr.playerStatus.GetComponent<PlayerStatus>().RemoveLife();
+            asteroidMgr.playerStatus.GetComponent<ShipController>().ResetPos();
+        }
+    }
+}
---
+++ Arteroids/Assets/_Hernan/Scripts/AsteroidsManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsManager : MonoBehaviour
+{
+    public int startAsteroidsAmount = 4;
+
+    public GameObject asteroidRef;
+
+    public GameObject bigAsteroid;
+    public GameObject midAsteroid;
+    public GameObject smallAsteroid;
+
+    private Vector2 randomPos;
+
+    public List<GameObject> allAsteroids;
+
+    public PlayerStatus playerStatus;
+
+    //Orden de Edge en Sentido Horario inicia a la Izquierda
+    private int edge = 0;
+
+    private float bounds_width;
+    private float bounds_height;
+
+    private void Start()
+    {
+        bounds_width = Screen.width/2;
+        bounds_height = Screen.height/2;
+    }
+
+    public void CreateNewAsteroid(Vector2 pos, Type t)
+    {
+        switch ((int)t)
+        {
+            case 0:
+                asteroidRef = bigAsteroid;
+                break;
+            case 1:
+                asteroidRef = midAsteroid;
+                break;
+            case 2:
+                asteroidRef = smallAsteroid;
+                break;
+        }
+
+        GameObject asteroidClone = Instantiate(asteroidRef, pos, Quaternion.identity, transform);
+        asteroidClone.GetComponent<Asteroid>().Initialize(t, pos, this);
+        allAsteroids.Add(asteroidClone);
+    }
+
+    public void CreateFirstAsteroids()
+    {
+
+        for (int i = 0; i < startAsteroidsAmount; i++)
+        {
+            randomPos = GetRandomPosOnEdge();
+            CreateNewAsteroid(randomPos, Type.Big);
+        }
+    }
+
+    public void RemoveAsteroid(GameObject asteroid) 
+    {
+        allAsteroids.Remove(asteroid);
+
+        if (allAsteroids.Count == 0)
+        {
+            ResetAsteroids();
+        }
+
+        for (int i = 0; i < allAsteroids.Count; i++)
+        {
+            if(allAsteroids[i] == null)
+            {
+                print("HAY UN MISING");
+                allAsteroids.Remove(allAsteroids[i]);
+            }
+        }
+    }
+
+    public void ResetAsteroids()
+    {
+        foreach (GameObject asteroid in allAsteroids)
+        {
+            Destroy(asteroid);
+        }
+
+        CreateFirstAsteroids();
+    }
+
+    public Vector2 GetRandomPosOnEdge()
+    {
+        float xPos = 0;
+        float yPos = 0;
+
+        switch (edge)
+        {
+            case 0: //Left
+                xPos = -bounds_width;
+                yPos = Random.Range(0, bounds_height);
+                break;
+            case 1: //Top
+                xPos = Random.Range(0, bounds_width); ;
+                yPos = bounds_height;
+                break;
+            case 2: //Right
+                xPos = bounds_width;
+                yPos = Random.Range(0, bounds_height);
+                break;
+            case 3: //Bot
+                xPos = Random.Range(0, bounds_width); ;
+                yPos = -bounds_height;
+                break;
+        }
+
+        edge++;
+
+        if (edge > 3)
+        {
+            edge = 0;
+        }
+
+        return new Vector2(xPos + bounds_width, yPos + bounds_height);
+    }
+}
