6970fdfc75cfd5d4a5debbcb9b5ce631e97d8df9
HernanFerrari <hernan.angel.ferrari@gmail.com>
2020-01-30 16:34:44 -0300

    Modificación de GameManager para incluir estados provisorios

--- Arteroids/Assets/_Hernan/Scripts/GameManager.cs
+++ Arteroids/Assets/_Hernan/Scripts/GameManager.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum State
+{
+    Menu,
+    Start,
+    InGame,
+    Score
+}
 
 public class GameManager : MonoBehaviour
 {
+    public State state;
+    public AsteroidsManager asteroidMgr;
+    public PlayerStatus playerStatus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Start;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch ((int)state)
+        {
+            case 0:
+                // menu
+                break;
+            case 1:
+                asteroidMgr.CreateFirstAsteroids();
+                state = State.InGame;
+                break;
+            case 2:
+                // InGame
+                break;
+            case 3:
+                // score
+                break;
+        }
     }
 }
